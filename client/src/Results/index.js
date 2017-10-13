6d43ae71cd77735a83c0458fb8833c759a96d9b4
dannycho7 <dannycho7@gmail.com>
2017-10-12 22:39:11 -0700

    Add Calendar, Search, and Results component infrastructure

---
+++ client/src/Results/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Results extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Place Results component here
+      </div>
+    );
+  }
+}
+
+export default Results;
