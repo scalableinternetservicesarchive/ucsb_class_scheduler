0a779191e20dbed8ac521d071afdf6ab6a3cae75
dannycho7 <dannycho7@gmail.com>
2017-10-12 22:38:59 -0700

    Set up base App component for Calendar, Results, and Search components

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Search from "./Search";
+import Calendar from "./Calendar";
+import Results from "./Results";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Calendar />
+        <Search />
+        <Results />
       </div>
     );
   }
