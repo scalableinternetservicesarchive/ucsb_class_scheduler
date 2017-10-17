a7c8caa25339c22929e178aa1a3bb6355b41d2cd
dannycho7 <dannycho7@gmail.com>
2017-10-16 20:59:55 -0700

    First pass results Table that displays prop data

--- client/src/Results/index.js
+++ client/src/Results/index.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Button from 'material-ui/Button';
+import Paper from 'material-ui/Paper';
 
 class Results extends Component {
   constructor(props) {
@@ -6,10 +9,31 @@ class Results extends Component {
   }
 
   render() {
+    const { results } = this.props;
+
     return (
-      <div>
-        Place Results component here
-      </div>
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Times</TableCell>
+              <TableCell>Options</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {results.map((result) => (
+                <TableRow>
+                  <TableCell>{result.name}</TableCell>
+                  <TableCell>{result.times}</TableCell>
+                  <TableCell>
+                    <Button raised color="accent">Add</Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
