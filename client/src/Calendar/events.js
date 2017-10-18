8aef1e91062b3159e419ffecf969ad0f10102c1d
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-10-17 19:05:51 -0700

    Add basic calendar

---
+++ client/src/Calendar/events.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    'title': 'Lunch',
+    'start':new Date(2015, 3, 12, 12, 0, 0, 0),
+    'end': new Date(2015, 3, 12, 13, 0, 0, 0),
+    desc: 'Power lunch'
+  },
+  {
+    'title': 'Meeting',
+    'start':new Date(2015, 3, 12,14, 0, 0, 0),
+    'end': new Date(2015, 3, 12,15, 0, 0, 0)
+  },
+  {
+    'title': 'Happy Hour',
+    'start':new Date(2015, 3, 12, 17, 0, 0, 0),
+    'end': new Date(2015, 3, 12, 17, 30, 0, 0),
+    desc: 'Most important meal of the day'
+  }
+]
--- client/src/Calendar/index.js
+++ client/src/Calendar/index.js
@@ -1,17 +1,29 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import BigCalendar from 'react-big-calendar';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from 'moment';
+import events from './events';
 
-class Calendar extends Component {
+
+BigCalendar.momentLocalizer(moment);
+
+export default class Calendar extends Component {
   constructor(props) {
-    super(props);
+    super(props)
+    this.state = { events: events };
+    BigCalendar.setLocalizer(
+      BigCalendar.momentLocalizer(moment)
+    );
   }
 
   render() {
     return (
       <div>
-        Place Calendar component here
+        <BigCalendar
+          events={this.state.events}
+          defaultView="week"
+        />
       </div>
     );
   }
 }
-
-export default Calendar;
