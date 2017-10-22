a81c76ce7e164bb01708ba3aad6598d5694044b5
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-10-21 18:54:34 -0700

    Delete mock data for Calendar

--- client/src/Calendar/index.js
+++ client/src/Calendar/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import BigCalendar from 'react-big-calendar';
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from 'moment';
-import events from './events';
 
 
 BigCalendar.momentLocalizer(moment);
