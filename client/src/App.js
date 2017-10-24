f1d3a2354e5c95e5afe274e6564185e796983980
dannycho7 <dannycho7@gmail.com>
2017-10-24 11:54:44 -0700

    Update HOC to pass down props that provide interactive data transfer between Search and Results components

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,33 @@ import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      term: ""
+    };
+
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+  }
+
+  onSearchSubmit(term) {
+    this.setState({ term });
   }
 
   render() {
+    const { term } = this.state;
+
     let results = [
       { name: "CMPSC 40", times: "M W 2:00 p.m - 3:15 p.m" },
       { name: "CMPSC 56", times: "T R 9:30 a.m - 10:45 a.m" },
       { name: "CMPSC 130A", times: "M W 9:30 a.m - 10:45 a.m" }
     ];
+
     return (
       <MuiThemeProvider>
         <div>
           <Calendar />
-          <Search />
-          <Results results={results} />
+          <Search onSubmit={this.onSearchSubmit} />
+          <Results filterTerm={term} results={results} />
         </div>
       </MuiThemeProvider>
     );
