99410497c64f87393c339d66199f755da3940cb9
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-11-04 21:59:26 -0700

    Add methods to create date object from course times

--- client/src/Results/Result.js
+++ client/src/Results/Result.js
@@ -3,21 +3,43 @@ import { TableRow, TableRowColumn } from 'material-ui/Table';
 import RaisedButton from 'material-ui/RaisedButton';
 
 const Result = ({ name, times, addEvent}) => {
-	let courseToEvent = () => {
-		// Return event object
-		return "newEvent"
+
+	let getCourseStart = (times) => {
+
 	}
 
-	let addCourseToCalendar = () => {
+	let getCourseEnd = (times) => {
+
+	}
+
+	let courseToEvent = (name, times) => {
+		/*
+		end: Thu Nov 02 2017 19:00:00 GMT-0700 (PDT)
+		start: Thu Nov 02 2017 17:00:00 GMT-0700 (PDT)
+		title: "Busy"
+		*/
+		let newEvent = new Object()
+		newEvent.title = name
+		newEvent.start = getCourseStart(times)
+		newEvent.end = getCourseEnd(times)
+		return newEvent
+	}
+
+	let addCourseToCalendar = (name, times) => {
 		console.log("Add new course");
-		addEvent("course")
+		let newEvent = courseToEvent(name, times)
+		addEvent(newEvent)
 	}
 	return (
 		<TableRow>
 			<TableRowColumn>{name}</TableRowColumn>
 			<TableRowColumn>{times}</TableRowColumn>
 			<TableRowColumn>
-				<RaisedButton label="Add" primary onClick={addCourseToCalendar} />
+				<RaisedButton
+					label="Add"
+					primary
+					onClick={addCourseToCalendar(name, times)}
+				/>
 			</TableRowColumn>
 		</TableRow>
 	);
