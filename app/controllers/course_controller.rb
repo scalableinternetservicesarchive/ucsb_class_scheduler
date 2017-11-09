e3ed6a15baf370dd21eff93de7a75acfeeeaf561
dannycho7 <dannycho7@gmail.com>
2017-11-09 12:10:46 -0800

    Add like functionality to Course Controller available t through /course/:id/like route

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -3,4 +3,11 @@ class CourseController < ApplicationController
 		@courses = Course.all
 		render json: @courses
 	end
+
+	def like
+		@like = Like.find_or_create_by(user: User.first, course_id: params[:id])
+		@like.amount += 1
+		@like.save!
+		render json: @like
+	end
 end
