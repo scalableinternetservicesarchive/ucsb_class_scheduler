b5745977618f72d431366673cd6938f821817988
dannycho7 <dannycho7@gmail.com>
2017-11-09 12:19:26 -0800

    Validate Like model for amount within the range [0, 10]

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,7 @@
 class Like < ApplicationRecord
 	belongs_to :course
 	belongs_to :user
+
+	validates_inclusion_of :amount, in: (0..10).to_a,
+		message: 'You cannot like a course more than 10 times'
 end
