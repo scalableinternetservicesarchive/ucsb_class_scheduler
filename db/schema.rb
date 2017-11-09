c47f0bfdfb88262244dc0b13177eb99fb30b87dd
dannycho7 <dannycho7@gmail.com>
2017-11-09 11:48:08 -0800

    Add users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101024810) do
+ActiveRecord::Schema.define(version: 20171109194411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,11 @@ ActiveRecord::Schema.define(version: 20171101024810) do
     t.string "location"
     t.bigint "course_id"
     t.index ["course_id"], name: "index_periods_on_course_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "courses", "instructors"
