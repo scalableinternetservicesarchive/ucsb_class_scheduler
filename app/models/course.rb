842c553aa32c8f80d01d5406d768e0285b496220
dannycho7 <dannycho7@gmail.com>
2017-11-15 18:35:32 -0800

    Add has_many comments relation to Course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,5 @@ class Course < ApplicationRecord
 	belongs_to :instructor
 	has_many :likes, dependent: :delete_all
 	has_many :periods, dependent: :destroy
+	has_many :comments, dependent: :destroy
 end
