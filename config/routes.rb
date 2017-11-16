78d98a23a6ab969841ee0cbc9ad7f59ad8b17c0f
dannycho7 <dannycho7@gmail.com>
2017-11-16 01:07:59 -0800

    Convert like a course action to be a POST request

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	get 'course/all', to: 'course#all'
-	get 'course/:id/like', to: 'course#like'
 	get 'course/:id', to: 'course#show'
+	post 'course/:id/like', to: 'course#like'
 	post 'course/:id/comment', to: 'course#comment'
 
 	get '*path', to: 'application#fallback_index_html', constraints: lambda { |request|
