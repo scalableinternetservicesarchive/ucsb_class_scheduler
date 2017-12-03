95e8c77c4edb872ab63c7138505615b0514cbd9f
dannycho7 <dannycho7@gmail.com>
2017-12-03 14:14:21 -0800

    Add MASTER_PROC flag on deploy-med script for worker process launch and static serve env var

--- scripts/deploy-med.sh
+++ scripts/deploy-med.sh
@@ -2,4 +2,4 @@
 
 time=`date +%Y%m%d%H%M%S`
 
-eb create -db.engine postgres -db.i db.m3.medium -db.user u --envvars SECRET_KEY_BASE=AAAAAA -i m3.medium $time
+eb create -db.engine postgres -db.i db.m3.medium -db.user u --envvars RAILS_SERVE_STATIC_FILES="true",SECRET_KEY_BASE=AAAAAA,MASTER_PROC=true -i m3.medium $time
