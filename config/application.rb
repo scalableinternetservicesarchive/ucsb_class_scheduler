ee0f1720676c50082ab84b2e6a2b237269b87e65
dannycho7 <dannycho7@gmail.com>
2017-12-03 20:52:56 -0800

    Add condition for scheduling job -- running process must be originally an application server

--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,8 @@ module UcsbClassScheduler
 		# Settings in config/environments/* take precedence over those specified here.
 		# Application configuration should go into files in config/initializers
 		# -- all .rb files in that directory are automatically loaded.
-
 		config.after_initialize do
-			if ENV['MASTER_PROC'] == 'true'
+			if ENV['MASTER_PROC'] == 'true' && (defined?(Rails::Server) || defined?(PhusionPassenger) || defined?(Puma))
 				AggregateCoursesJob.perform_now
 				exec('MASTER_PROC=false bundle exec clockwork lib/clock.rb') if fork.nil?
 			end
